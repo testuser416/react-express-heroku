3693f41560e9ef3b177f8a25a72b5ec95661d923
testuser416 <84838912+testuser416@users.noreply.github.com>
2021-11-16 15:36:10 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,53 @@
+const path = require("path");
+const express = require("express");
+const app = express();
+const multer = require("multer");
+const fileStorage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "images");
+  },
+  filename: (req, file, cb) => {
+    cb(null, new Date().toISOString() + "-" + file.originalname);
+  },
+});
+
+const fileFilter = (req, file, cb) => {
+  if (
+    file.mimetype === "image/png" ||
+    file.mimetype === "image/jpg" ||
+    file.mimetype === "image/jpeg"
+  ) {
+    cb(null, true);
+  } else {
+    cb(null, false);
+  }
+};
+const mongoose = require("mongoose");
+const feedRoutes = require("./routes/feed");
+const bodyParser = require("body-parser");
+app.use(
+  multer({ storage: fileStorage, fileFilter: fileFilter }).single("image")
+);
+app.use(bodyParser.json()); // application/json
+app.use("/images", express.static(path.join(__dirname, "images")));
+app.use(express.static(path.join(__dirname, "build")));
+app.get("/*", (req, res) => {
+  res.sendFile(path.join(__dirname, "build", "index.html"));
+});
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "POST,GET,PUT,DELETE");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type,Authorization");
+  next();
+});
+app.use(feedRoutes);
+mongoose
+  .connect(
+    "mongodb+srv://mateen:Y9j9wR30JZNevcdN@cluster0.crgnu.mongodb.net/feedposts?retryWrites=true&w=majority"
+  )
+  .then((result) => {
+    app.listen(4000);
+  })
+  .catch((error) => {
+    console.log(error);
+  });
---
+++ build/precache-manifest.98f5a9f3ac6ce6a066c2d39352b4de65.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "3a9da449e4c15d64db56",
+    "url": "/static/css/main.dfa27b62.chunk.css"
+  },
+  {
+    "revision": "3a9da449e4c15d64db56",
+    "url": "/static/js/main.3a9da449.chunk.js"
+  },
+  {
+    "revision": "d9fd9a03b80d046a6111",
+    "url": "/static/js/1.d9fd9a03.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "917c16d4e68a6cb947977b7a498277bc",
+    "url": "/index.html"
+  }
+];
